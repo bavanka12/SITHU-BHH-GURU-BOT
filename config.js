b2274aeeeb1b604a5ebaf48c5d63d42e30034f20
bavanka12 <maneeshapahasara26@gmail.com>
2024-11-26 13:30:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94760334624', 'me', true],
+  ['94760334624', 'sithu bhh', true], 
   [''] 
 ] //Number of owners
 
